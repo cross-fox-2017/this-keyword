ec9ff21d3704b6eec86492041cb88289ca19c1c4
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-09 11:01:49 +0700

    release 1 done

--- this.js
+++ this.js
@@ -20,3 +20,5 @@ let v = new nodeVersion()
 
 console.log(v.versi());
 // RELEASE 1
+//this di class berfungsi untuk mendeklrasikan public properti / private
+//dan didalam class this juga tidak bisa langsung dipanggil , harus di return didalam method class

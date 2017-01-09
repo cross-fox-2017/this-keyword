f4b7463f930cb0e3fee8288ac1514544ec409d01
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-09 10:52:36 +0700

    class context done

--- this.js
+++ this.js
@@ -7,7 +7,16 @@ console.log(process.versions.node);
 
 
 // CLASS CONTEXT
+class nodeVersion {
+  constructor() {
+    this.version = process.versions.node
+  }
+  versi(){
+    return this.version
+  }
+}
 
+let v = new nodeVersion()
 
-
+console.log(v.versi());
 // RELEASE 1

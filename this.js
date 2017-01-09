dcb4f92b58c9ade4ac95e98af8009f47acc901d5
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-09 10:52:05 +0700

    global contex done

--- this.js
+++ this.js
@@ -2,10 +2,23 @@
 
 // GLOBAL CONTEXT
 
+console.log(process.versions.node);
 
 
 
 // CLASS CONTEXT
 
 
+class nodeVersion {
+  constructor() {
+    this.version = process.versions.node
+  }
+  versi(){
+    return this.version
+  }
+}
+
+let v = new nodeVersion()
+
+console.log(v.versi());
 // RELEASE 1

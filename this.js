29f938b131e483630abde9d57e7b2dfa11b51547
unknown <yaumashf@gmail.com>
2017-01-09 10:52:15 +0700

    version node using this

--- this.js
+++ this.js
@@ -1,8 +1,19 @@
 "use strict"
 
 // GLOBAL CONTEXT
+// console.log(process.versions.node);
+class versi_js {
+  constructor() {
+    this.versions = process.versions.node;
+  }
+  versi(){
+    return this.versions;
+  }
+}
 
+var versiiiii = new versi_js();
 
+console.log(versiiiii.versi());
 
 
 // CLASS CONTEXT

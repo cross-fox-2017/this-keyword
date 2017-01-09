f8b0599e882247fb2fb81d67db5e036a2d76e31d
unknown <yaumashf@gmail.com>
2017-01-09 16:01:32 +0700

    release 1

--- this.js
+++ this.js
@@ -18,5 +18,8 @@ console.log(versiiiii.versi());
 
 // CLASS CONTEXT
 
-
 // RELEASE 1
+// this dapat digunakan di dalam konstruktor untuk memanggil konstruktor yang lain dari kelas yang sama
+// ketika digunakan pada fungsi, nilai dari this adalah objek dari fungsi itu sendiri
+// ketika pada objek, nilainya adalah dirinya sendiri
+// this pada objek constructor tidak memiliki nilai, hanya menggantikan untuk objek baru

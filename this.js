9553066c8541791a292a7e1d31a753b3f7ed34b2
Eri Irawan <email": null>
2017-01-09 15:07:08 +0700

    this keyword done

--- this.js
+++ this.js
@@ -2,11 +2,27 @@
 
 // GLOBAL CONTEXT
 /*
-  Method process itu adalah untuk melihat version
+  Untuk melihat version pertama menggunakan syntax 'node' terlebih daulu, kemudian
+  menuliskan process.version => (node.process.version). Dan untuk melihat method -  method
+  yang ada dinode menggunakan syntax 'this'.
 */
 
 
 // CLASS CONTEXT
 
+class people {
+  constructor (name){
+    this.resultName = name;
+  }
+}
+
+let resultPeople = new people ("Eri Irawan");
+console.log(resultPeople.resultName);
 
 // RELEASE 1
+/*
+  - parameter yang menggunakan this bisa dipanggil difungsi yang lain, karena sifat dari this
+    adalah global.
+  - apabila this digunakan di object, this bisa dipakai untuk memanggil properties object tersebut.
+  - karena this sifat nya global sehingga this dapat berfungsi untuk mengembangkan nilai properties.
+*/
